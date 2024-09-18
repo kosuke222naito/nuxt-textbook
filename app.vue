3231d27adee2a0284480e17bf46b837b54a59990
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-09-18 11:43:03 +0900

    add member list page

--- app.vue
+++ app.vue
@@ -1,6 +1,53 @@
+<script setup lang="ts">
+import OneMember from "./components/OneMember.vue";
+
+interface Member {
+  id: number;
+  name: string;
+  email: string;
+  points: number;
+  note?: string;
+}
+
+const memberListInit = new Map<number, Member>();
+
+memberListInit.set(33456, {
+  id: 33456,
+  name: "田中太郎",
+  email: "taro@example.com",
+  points: 30,
+  note: "ほげほげ",
+});
+
+memberListInit.set(47783, {
+  id: 47783,
+  name: "鈴木二郎",
+  email: "jiro@example.com",
+  points: 300,
+  note: "ふがふが",
+});
+
+const memberList = ref(memberListInit);
+
+const totalPoints = computed(() => {
+  return [...memberList.value.values()].reduce((sum, member) => {
+    return sum + member.points;
+  }, 0);
+});
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <section>
+    <h1>会員リスト</h1>
+    <p>全会員の合計保有ポイント: {{ totalPoints }}</p>
+    <OneMember
+      v-for="[id, { name, email, points, note }] in memberList"
+      :id
+      :key="id"
+      :name
+      :email
+      :points
+      :note
+    />
+  </section>
 </template>
---
+++ components/OneMember.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+interface Props {
+  id: number;
+  name: string;
+  email: string;
+  points: number;
+  note?: string;
+}
+
+const { id, name, email, points, note = "--" } = defineProps<Props>();
+const pointsDisplay = ref(points);
+
+const pointUp = (): void => {
+  pointsDisplay.value++;
+};
+</script>
+
+<template>
+  <section class="box">
+    <h4>{{ name }}さんの情報</h4>
+    <dl>
+      <dt>ID</dt>
+      <dd>{{ id }}</dd>
+      <dt>名前</dt>
+      <dd>{{ name }}</dd>
+      <dt>メールアドレス</dt>
+      <dd>{{ email }}</dd>
+      <dt>保有ポイント</dt>
+      <dd>{{ pointsDisplay }}</dd>
+      <dt>備考</dt>
+      <dd>{{ note }}</dd>
+    </dl>
+    <button @click="pointUp">ポイント加算</button>
+  </section>
+</template>
+
+<style scoped>
+.box {
+  border: green 1px solid;
+  margin: 10px;
+}
+</style>
