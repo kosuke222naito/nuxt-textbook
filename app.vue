d3eeed2ded348fe2f88b1e40e69a15985c6a0c74
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-09-18 15:33:04 +0900

    use NuxtPage

--- app.vue
+++ app.vue
@@ -1,6 +1,63 @@
+<script setup lang="ts">
+import type { Member } from "~/interfaces";
+
+const memberListInit = new Map<number, Member>();
+
+memberListInit.set(33456, {
+  id: 33456,
+  name: "田中太郎",
+  email: "taro@example.com",
+  points: 30,
+});
+
+memberListInit.set(47783, {
+  id: 47783,
+  name: "鈴木二郎",
+  email: "jiro@example.com",
+  points: 300,
+  note: "ふがふが",
+});
+
+useState<Map<number, Member>>("memberList", () => memberListInit);
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <header>
+    <h1>ルーティングサンプル</h1>
+    <main>
+      <NuxtPage />
+    </main>
+  </header>
 </template>
+
+<style scoped>
+main {
+  border: blue 1px solid;
+  padding: 10px;
+}
+
+#breadcrumbs ul li {
+  display: inline;
+  list-style-type: none;
+}
+
+#breadcrumbs {
+  margin-left: 0px;
+}
+
+#breadcrumbs ul {
+  padding-left: 0px;
+}
+
+#breadcrumbs ul .current {
+  color: red;
+}
+
+#breadcrumbs ul li:before {
+  content: " > ";
+}
+
+#breadcrumbs ul li:first-child:before {
+  content: none;
+}
+</style>
